711b6a69f58205ff024c1844880372f2d0b40675
wellyton-xs <wellyton.offcer@gmail.com>
2024-09-26 16:25:15 -0300

    add flexibility and checking for updates

--- install.sh
+++ install.sh
@@ -4,18 +4,39 @@ print(){
 	printf '%s\n' "$1"
 }
 
+download_latest(){
+    curl https://raw.githubusercontent.com/wellyton-xs/archiso_installer/refs/heads/main/install.sh -o "$1"
+}
+
+check-updates(){
+    download_latest /tmp/install.sh
+    diff install.sh /tmp/install.sh
+    if [["$?" == 1]]; then
+	download_latest .
+    elif [["$?" == 0]]; then
+	print "everything is up to date! have a good installation"
+    fi
+}
+
+check-updates
+
 EFI="/dev/vda1"
 SWAP="/dev/vda2"
 ROOT="/dev/vda3"
 
-print "define keyboard"
-loadkeys br-abnt2
+read -p "type your keyboard" KEYBOARD
+read -p "choose system language enconding" LANGUAGE
+read -p "choose your zone" ZONEINFO
+read -p "set root password" ROOTPASSWD
+
+print "setting keyboard"
+loadkeys "$KEYBOARD"
 
 print "getting platform size"
 cat /sys/firmware/efi/fw_platform_size
 
-print "please wait 10s and create your partitions:"
-sleep 10
+print "please wait 5s and create your partitions:"
+sleep 5
 cfdisk
 
 print "formating root"
@@ -46,30 +67,29 @@ genfstab -U /mnt >> /mnt/etc/fstab
 
 print "join into system with arch-chroot"
 arch-chroot /mnt /bin/bash <<EOF
-
 printf  "%s\n" "set zoneinfo to localtime file"
-ln -sf /usr/share/zoneinfo/America/Bahia /etc/localtime
+ln -sf $ZONEINFO /etc/localtime
 hwclock --systohc
 
 printf "%s\n" "creating locale.gen"
-sed -i '/^#en_US.UTF-8/s/^#//' /etc/locale.gen
-sed -i '/^#pt_BR.UTF-8/s/^#//' /etc/locale.gen
+#sed -i '/^#en_US.UTF-8/s/^#//' /etc/locale.gen
+sed -i "/^#$ENCODING/s/^#//" /etc/locale.gen
 locale-gen
 
 printf "%s\n" "generate locale.conf"
-echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
+echo "LANG=$ENCODING" > /etc/locale.conf
 
 printf "%s\n" "setting up keyboard"
-echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
+echo "KEYMAP=$KEYBOARD" > /etc/vconsole.conf
 
 printf "%s\n" "create user"
-echo "welly" > /etc/hostname
+echo "$HOSTNAME" > /etc/hostname
 
 printf "%s\n" "running mkinitcpio"
 mkinitcpio -P
 
 printf "%s\n" "choose your password"
-passwd
+echo "root:$ROOTPASSWD" | chpasswd
 
 printf "%s\n" "installing bootloader"
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
